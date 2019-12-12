ed3893a5adc22f4b165c7472f798d40f351af697
Ben F <56423163+bferriman@users.noreply.github.com>
2019-12-12 17:25:34 -0500

    completed README

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # day-planner
+
+Check out the [deployed page](https://bferriman.github.io/day-planner/)!
+
+## Description
+
+This page lets users schedule daily tasks and appointments on a 9:00 - 5:00 calendar.
+
+## Features
+
+Current day and time are displayed dynamically.
+
+Each hour-long time block is color-coded to indicate its relative position to the current time.
+
+User inputs are stored in localStorage and retrieved when they make subsequent visits to the page.
+
+Responsive to changing screen size.
+
+Dynamically displays button controls appropriate to the state of each time block.
+
+## Opportunities For Further Development
+
+I sometimes wish that hitting the enter key with focus on a textarea would trigger the Save button. On the other hand, that takes away text entry flexibility.
+
+The app would be more useful if it allowed the user to save events for more than one day at a time.
+
+The app could provide some kind of alert when a scheduled event is approaching.
+
+## Credits
+
+This site uses [Bootstrap 4](https://getbootstrap.com/) (copyright 2019 Twitter).
+
+Icons are from [Font Awesome](https://fontawesome.com/).
+
+Color scheme chosen with the help of [Colormind](http://colormind.io/).
+
+## Reference Sources Used
+
+[w3schools.com](https://www.w3schools.com/)
+
+[jQuery](https://jquery.com/)
+
+
+## License
+
+Copyright (c) Benjamin Ferriman. All rights reserved.
+Licensed under the [MIT](https://github.com/bferriman/portfolio/blob/master/LICENSE.txt) license.
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,3 @@
-$(document).ready(function() {
-
-
-
 function initializePage() {
     
     loadTime();
@@ -10,55 +6,55 @@ function initializePage() {
     colorRows();
 }
 
+$(document).ready(function() {
 
-initializePage();
+    initializePage();
 
+    $(document).on("click", ".buttons-row", function(event){  //listens for clicks in the div holding the buttons
 
-$(document).on("click", ".buttons-row", function(event){  //listens for clicks in the div holding the buttons
+        //grab the clicked button or return if a button wasn't clicked
+        var clickTargetName = event.target.nodeName;
+        var targetButton;
 
-    //grab the clicked button or return if a button wasn't clicked
-    var clickTargetName = event.target.nodeName;
-    var targetButton;
+        if(clickTargetName === "BUTTON"){  //click was on button element
+            targetButton = $(event.target);
+        }
+        else if (clickTargetName === "I"){  //click was on icon inside button
+            targetButton = $(event.target).parent();
+        }
+        else{  //click was not on a button
+            return;
+        }
 
-    if(clickTargetName === "BUTTON"){  //click was on button element
-        targetButton = $(event.target);
-    }
-    else if (clickTargetName === "I"){  //click was on icon inside button
-        targetButton = $(event.target).parent();
-    }
-    else{  //click was not on a button
-        return;
-    }
+        //get button type
+        var buttonType = targetButton.attr("data-buttonType");
 
-    //get button type
-    var buttonType = targetButton.attr("data-buttonType");
+        //switch - call functions to handle button types, passing clicked buttons-row element
+        switch(buttonType){
 
-    //switch - call functions to handle button types, passing clicked buttons-row element
-    switch(buttonType){
+            case "add":
+                handleAddButton($(this));
+                break;
 
-        case "add":
-            handleAddButton($(this));
-            break;
+            case "edit":
+                handleEditButton($(this));
+                break;
 
-        case "edit":
-            handleEditButton($(this));
-            break;
+            case "delete":
+                handleDeleteButton($(this));
+                break;
 
-        case "delete":
-            handleDeleteButton($(this));
-            break;
+            case "save":
+                handleSaveButton($(this));
+                break;
 
-        case "save":
-            handleSaveButton($(this));
-            break;
+            case "cancel":
+                handleCancelButton($(this));
+                break;
 
-        case "cancel":
-            handleCancelButton($(this));
-            break;
-
-        default:
-            console.log("ERROR: Unexpected button type");
-    }
-});
+            default:
+                console.log("ERROR: Unexpected button type");
+        }
+    });
 
 });
