a645a4abbb0c9266f73800df881301de0d584406
Ben F <56423163+bferriman@users.noreply.github.com>
2019-12-12 01:01:19 -0500

    coded event listener and tidied up button creation code to be more dry

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,9 @@ var dateEl = $("#date");
 var containerDiv = $("#main");
 var timeRowArr = [];
 
+var hourStrings = ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"];
+
+
 
 function displayTime(x) {
     if (x.matches) { // screen is xs or sm
@@ -29,8 +32,6 @@ function loadTime() {
 
 function buildSkeleton() {
 
-    var hourStrings = ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"];
-
     for(var i = 0; i < hourStrings.length; i++) {
 
         var timeRow = $("<div>");
@@ -48,26 +49,17 @@ function buildSkeleton() {
         // textAreaDiv.append(textArea);
         var textDisplay = $("<div>");
         textDisplay.attr("class", "px-2");
+        textDisplay.attr("data-index", i);
         textAreaDiv.append(textDisplay);
         timeRow.append(textAreaDiv);
 
         var buttonsColDiv = $("<div>");
         buttonsColDiv.attr("class", "col-2 bg-info round-right")
         var buttonsRowDiv = $("<div>");
-        buttonsRowDiv.attr("class", "row");
-        var buttonDiv1 = $("<div>");
-        buttonDiv1.attr("class", "col-lg-6 px-0");
-        var button1 = $("<button>");
-        button1.attr("class", "btn bg-white d-block mx-auto p-0 action-button add-button");
-        button1.html("<i class='fas fa-save'></i>");
-        buttonDiv1.append(button1);
-        var buttonDiv2 = $("<div>");
-        buttonDiv2.attr("class", "col-lg-6 px-0");
-        var button2 = $("<button>");
-        button2.attr("class", "btn bg-white d-block mx-auto p-0 action-button add-button");
-        button2.html("<i class='fas fa-ban'></i>");
-        buttonDiv2.append(button2);
-        buttonsRowDiv.append(buttonDiv1, buttonDiv2);
+        buttonsRowDiv.attr("class", "row buttons-row");
+
+        makeButton("add", buttonsRowDiv);
+
         buttonsColDiv.append(buttonsRowDiv);
         timeRow.append(buttonsColDiv);
 
@@ -77,8 +69,62 @@ function buildSkeleton() {
     }
 }
 
+function makeButton(buttonType, parentDiv) {  //not currently in use - just saving code
+   
+    var buttonDiv = $("<div>");
+    if(buttonType === "add"){
+        buttonDiv.attr("class", "col px-0");
+    }
+    else {
+        buttonDiv.attr("class", "col-lg-6 px-0");
+    }
+    var button = $("<button>");
+    button.attr("class", "btn bg-white d-block mx-auto p-0 action-button");
+    button.attr("data-buttonType", buttonType);
+    
+    switch(buttonType) {
+        
+        case "add":
+            button.html("<i class='fas fa-plus'></i>");
+            break;
+
+        case "edit":
+            button.html("<i class='fas fa-edit'></i>");
+            break;
+
+        case "delete":
+            button.html("<i class='fas fa-trash'></i>");
+            break;
+
+        case "save":
+            button.html("<i class='fas fa-save'></i>");
+            break;
+
+        case "cancel":
+            button.html("<i class='fas fa-ban'></i>");
+            break;
+
+        default:
+            console.log("ERROR: Unexpected button type");
+
+    }  //end switch
+
+    buttonDiv.append(button);
+   
+    parentDiv.append(buttonDiv);
+}  //end makeButton function
+
 function loadData() {  //fetch data from local storage and display in appropriate elements
     console.log("loadData function called");
+    var data;
+
+    for(var i = 0; i < hourStrings.length; i++){
+        console.log("iterating..." + i);
+        data = localStorage.getItem(i);
+        if (data !== null){
+            $("#rowNum" + i).text(data);
+        }
+    }
 }
 
 function initializePage() {
@@ -88,23 +134,91 @@ function initializePage() {
     loadData();
 }
 
+function handleAddButton(buttonRow) {
+    console.log("add button clicked");
+    //grab text div
+    var textDiv = buttonRow.parent().prev();
+    //delete its child div
+    textDiv.empty();
+    //add an empty textarea element
+    var textArea = $("<textarea>");
+    textArea.attr("class", "w-100 h-100 px-2 no-resize");
+    textDiv.append(textArea);
+    //delete button div's child elements
+    buttonRow.empty();
+    //build save and cancel buttons
+    makeButton("save", buttonRow);
+    makeButton("cancel", buttonRow);
+}
+
+function handleEditButton(buttonRow) {
+    console.log("edit button clicked");
+}
+
+function handleDeleteButton(buttonRow) {
+    console.log("delete button clicked");
+}
+
+function handleSaveButton(buttonRow) {
+    console.log("save button clicked");
+}
+
+function handleCancelButton(buttonRow) {
+    console.log("cancel button clicked");
+}
+
+
+
 initializePage();
 
+
 //add event listeners for all buttons
-$(".add-button").on("click", function(){
-    console.log("an Add button has been clicked!");
+$(document).on("click", ".buttons-row", function(event){
+
+    //grab the clicked button or return if a button wasn't clicked
+    var clickTargetName = event.target.nodeName;
+    var targetButton;
+
+    if(clickTargetName === "BUTTON"){  //click was on button element
+        targetButton = $(event.target);
+    }
+    else if (clickTargetName === "I"){  //click was on icon inside button
+        targetButton = $(event.target).parent();
+    }
+    else{  //click was not on a button
+        return;
+    }
+
+    //get button type
+    var buttonType = targetButton.attr("data-buttonType");
+
+    //switch - call functions to handle button types, passing clicked buttons-row element
+    switch(buttonType){
+
+        case "add":
+            handleAddButton($(this));
+            break;
+
+        case "edit":
+            handleEditButton($(this));
+            break;
+
+        case "delete":
+            handleDeleteButton($(this));
+            break;
+
+        case "save":
+            handleSaveButton($(this));
+            break;
+
+        case "cancel":
+            handleCancelButton($(this));
+            break;
+
+        default:
+            console.log("ERROR: Unexpected button type");
+    }
 });
-$(".edit-button").on("click", function(){
-    console.log("an Edit button has been clicked!");
-});
-$(".delete-button").on("click", function(){
-    console.log("a Delete button has been clicked!");
-});
-$(".save-button").on("click", function(){
-    console.log("a Save button has been clicked!");
-});
-$(".cancel-button").on("click", function(){
-    console.log("a Cancel button has been clicked!");
-});
+
 
 });
